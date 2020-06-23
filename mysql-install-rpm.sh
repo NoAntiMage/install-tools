b1617307fed96879132eafcd2b5bda3f8fc26c59
root <395307259@qq.com>
2020-06-23 15:00:45 +0800

    mysql_rpm_install

---
+++ mysql-install-rpm.sh
@@ -0,0 +1,39 @@
+# 获取安装包
+yum install -y wget
+http://mirror.centos.org/centos/6/os/x86_64/Packages/libaio-0.3.107-10.el6.x86_64.rpm
+rpm -ivh libaio-0.3.107-10.el6.x86_64.rpm
+
+wget http://www.cpan.org/src/5.0/perl-5.16.1.tar.gz
+tar -xzf perl-5.16.1.tar.gz
+cd perl-5.16.1
+./Configure -des -Dprefix=/usr/local/perl
+make && make test && make install
+perl -v
+
+wget https://cdn.mysql.com//Downloads/MySQL-5.7/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar
+
+# 删除mariadb
+rpm -e --nodeps $(rpm -qa | grep mariadb)
+
+# 解压安装(注意安装顺序)
+tar -xvf mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar
+rpm -ivh mysql-community-common-5.7.28-1.el7.x86_64.rpm
+rpm -ivh mysql-community-libs-5.7.28-1.el7.x86_64.rpm
+rpm -ivh mysql-community-libs-compat-5.7.28-1.el7.x86_64.rpm
+rpm -ivh mysql-community-client-5.7.28-1.el7.x86_64.rpm
+rpm -ivh mysql-community-server-5.7.28-1.el7.x86_64.rpm
+rpm -ivh mysql-community-devel-5.7.28-1.el7.x86_64.rpm
+
+# 数据库初始化
+mysqld --initialize --user=mysql
+
+# 获取root初始密码
+cat /var/log/mysqld.log | grep temporary
+
+# 启动mysql
+systemctl start mysqld.service && systemctl enable mysqld.service
+systemctl status mysqld.service
+
+# ALTER USER USER() IDENTIFIED BY '$PASSWORD';
+
+# 主从配置

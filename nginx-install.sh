ea0b8e7ca08f8eb6bb3c26b85efd928c3bf09d54
rpmaker <wujiwust@gmail.com>
2020-10-16 10:33:01 +0800

    nginx-tar-install

--- nginx-install.sh
+++ nginx-install.sh
@@ -5,9 +5,22 @@ yum install -y zlib
 yum install -y zlib-devel
 yum install -y openssl
 yum install -y openssl-devel
+
+#rpm install 
 rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
 yum install -y nginx
 systemctl start nginx
 systemctl enable nginx
 systemctl status nginx
+
+#configure install
+WORKDIR='/usr/local/nginx1.18'
+wget https://nginx.org/download/nginx-1.18.0.tar.gz
+tar -zxvf nginx-1.18.0.tar.gz
+cd nginx-1.18.0
+./configure --prefix=${WORKDIR} 
+make && make install
+echo 'PATH='${WORKDIR}'sbin/:$PATH' > /etc/profile
+source /etc/profile
+
 
